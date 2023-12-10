2620949e66d2dfd35306556d0d8e5920372a7be0
Liming <huangliming1000@163.com>
2023-12-10 13:58:10 -0500

    new

--- starter/public/js/login.js
+++ starter/public/js/login.js
@@ -4,18 +4,6 @@ import { showAlert } from './alert.js';
 // const axios = require('axios/dist/browser/axios.cjs');
 // import axios from 'axios';
 
-const passwordElement = document.getElementById('password');
-const emailElement = document.getElementById('email');
-
-if (loginForm && passwordElement && emailElement) {
-  loginForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const password = passwordElement.value;
-    const email = emailElement.value;
-    login(email, password);
-  });
-}
-
 const login = async (email, password) => {
   try {
     const res = await axios({
@@ -53,40 +41,3 @@ if (loginForm) {
     login(email, password);
   });
 }
-
-// import axios from 'axios';
-
-// export const login = async (email, password) => {
-//   try {
-//     const res = await axios({
-//       method: 'POST',
-//       url: 'http://127.0.0.1:3000/api/v1/users/login',
-//       data: {
-//         email,
-//         password
-//       } //向api发送一个post请求
-//     });
-//     if (res.data.status === 'success') {
-//       alert('logged in successfully');
-//       window.setTimeout(() => {
-//         location.assign('/');
-//       }, 1500); //1.5s以后自动跳转首页
-//     }
-//   } catch (err) {
-//     alert(err.response.data.message);
-//   }
-// };
-
-// const hideAlert = () => {
-//   const el = document.querySelector('.alert');
-//   if (el) {
-//     el.parentElement.removeChild(el);
-//   }
-// };
-
-// const showAlert = (type, msg) => {
-//   const markup = `<div class="alert alert--${type}">${msg}</div>`;
-//   document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
-//   window.setTimeout(hideAlert, 5000);
-// };
-
